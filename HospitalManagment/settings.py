fb1ac80464f00101c1cdc1f102192fba9a67e9f9
Yeasin Arafat Nayem <87275236+yanayem@users.noreply.github.com>
2025-11-21 14:53:28 +0600

    Configure ALLOWED_HOSTS for production environment
    Updated ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS for production.

--- HospitalManagment/settings.py
+++ HospitalManagment/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-*vqjb^gik*y9ibp^as8fi5=+(al2p)rztlo9q#pz&6no^fowg-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = []
-#ALLOWED_HOSTS = ["hospital-appointment-and-patient.onrender.com"]
+ALLOWED_HOSTS = ["hospital-appointment-and-patient.onrender.com"]
 CSRF_TRUSTED_ORIGINS = [ "https://hospital-appointment-and-patient.onrender.com",]
 CSRF_TRUSTED_ORIGINS = [
    "https://hospital-appointment-and-patient.onrender.com",
